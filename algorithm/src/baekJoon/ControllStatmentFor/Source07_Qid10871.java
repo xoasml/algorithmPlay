371cc4b76d1dfa53f0fb0e284308220f6888e35d
TaeHoonKim <xoasml@gmail.com>
2020-12-16 23:51:20 +0900

    2020.12.16 for

---
+++ algorithm/src/baekJoon/ControllStatmentFor/Source07_Qid10871.java
@@ -0,0 +1,22 @@
+package baekJoon.ControllStatmentFor;
+
+import java.util.Scanner;
+
+public class Source07_Qid10871 {
+	public static void main(String[] args){
+    	Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int x = scan.nextInt();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 1 ; i <= n ; i++) {
+			
+			int fn = scan.nextInt();
+			if(fn < x) {
+				sb.append(fn+" ");
+			}
+		}
+		System.out.println(sb);
+	}
+}
