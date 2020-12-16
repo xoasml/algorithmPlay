62a8b1dbea50b49151057aaf0a765b94130b7b5e
TaeHoonKim <xoasml@gmail.com>
2020-12-16 23:39:55 +0900

    2020.12.16 for

---
+++ algorithm/src/baekJoon/ControllStatmentFor/Source05_QId11021.java
@@ -0,0 +1,23 @@
+package baekJoon.ControllStatmentFor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * 각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
+ */
+
+public class Source05_QId11021 {
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i = 1 ; i <= t ; i++) {
+			System.out.printf("Case #%d: %d%n", i, Integer.parseInt(br.readLine())+Integer.parseInt(br.readLine()) );
+		}
+	
+	}
+}
---
+++ algorithm/src/baekJoon/ControllStatmentFor/Source06_QId11022.java
@@ -0,0 +1,9 @@
+package baekJoon.ControllStatmentFor;
+
+
+/*
+ * 각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+ */
+public class Source06_QId11022 {
+
+}
