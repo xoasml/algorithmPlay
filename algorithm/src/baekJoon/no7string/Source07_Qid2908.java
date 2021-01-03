29989684ce0626e8cab2d3feceec240be797fd60
TaeHoonKim <xoasml@gmail.com>
2021-01-04 00:19:47 +0900

    2021.01.04 commit

--- algorithm/src/baekJoon/no7string/Source07_Qid2908.java
+++ algorithm/src/baekJoon/no7string/Source07_Qid2908.java
@@ -8,6 +8,19 @@ public class Source07_Qid2908 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] inputArr = br.readLine().trim().split(" ");
+		StringBuilder sb;
+		int iArr[] = new int[2];
 		
+		for(int i = 0 ; i < inputArr.length ; i++) {
+			sb = new StringBuilder();
+			sb.append(inputArr[i]);
+			iArr[i] = Integer.parseInt(sb.reverse().toString());
+		}
+		
+		if(iArr[0] > iArr[1]) {
+			System.out.println(iArr[0]);
+		} else {
+			System.out.println(iArr[1]);
+		}
 	}
 }
